09d0e39db33c0e30e18d4af4d96b443a7d4fa188
Zach Resnick <zachary.l.resnick@gmail.com>
2019-11-20 13:53:19 -0500

    Feat: Affinity tracker works for main categories

---
+++ extension/productPage.js
@@ -0,0 +1,23 @@
+let path = document.getElementsByClassName('c-breadcrumb')[0].outerText;
+
+let category = path.split(' ')[0];
+
+const addToStorage = cat => {
+  if (cat === "Women's") cat = 'Womens';
+  if (cat === "Men's") cat = 'Mens';
+  window.localStorage[cat]
+    ? window.localStorage[cat]++
+    : (window.localStorage[cat] = 1);
+};
+addToStorage(category);
+
+let affinity = {
+  Mens: window.localStorage.Mens,
+  Womens: window.localStorage.Womens,
+  Beauty: window.localStorage.Beauty,
+  Home: window.localStorage.Home,
+  Lifestyle: window.localStorage.Lifestyle,
+};
+console.log('Chrome Extension Output!');
+console.log('Category:  ', category);
+console.log('Affinity:  ', affinity);
