0e57f9b856d5e2089ab82e0801ebe83c18cb140e
Zach Resnick <zachary.l.resnick@gmail.com>
2019-11-21 13:31:46 -0500

    Updates comments

--- extension/newArrivals.js
+++ extension/newArrivals.js
@@ -1,9 +1,9 @@
-//get the parent node that holds all the sections
+//Get the parent node that holds all the category sections.
 let modules = Array.from(
   document.getElementsByClassName('dom-landing-page-modules')[0].childNodes
 ).filter(node => node.className === 'o-row   ');
 
-//get each section and assign their innerHTML to an object, refs, for easy reference
+//Get each section and assign their innerHTML to an object, refs, for easy reference.
 let mens = modules.filter(node => node.innerText.includes("Men's"));
 let womens = modules.filter(node => node.innerText.includes("Women's"));
 let beauty = modules.filter(node => node.innerText.includes('Beauty'));
@@ -18,7 +18,7 @@ let refs = {
   Home: home[0].innerHTML,
 };
 
-// get the affinities from local storage and sort them into a sorted array, sorted.  If no affinity exists or only some of the affinities exist, organize the rest according to Urban Outfitters original layout
+// get the affinities from local storage and sort them into a sorted array, sorted.  If no affinity exists or only some of the affinities exist, organize the rest according to Urban Outfitters original layout.  To do this, I set default values according to the original layout for each category if no affinity exists.
 let affinity = {
   Mens: window.localStorage["Men's"] ? window.localStorage["Men's"] : 0.8,
   Womens: window.localStorage["Women's"] ? window.localStorage["Women's"] : 0.9,
@@ -29,16 +29,17 @@ let affinity = {
     : 0.6,
 };
 
+//sort the array by affinity value
 let sorted = [];
 for (let prop in affinity) {
   sorted.push([prop, affinity[prop]]);
 }
-
 sorted.sort(function(a, b) {
   return a[1] - b[1];
 });
 
-// attach them to the DOM in order
+// Attach them to the DOM in order.  To do this, I iterate through all the modules found at the top of the page.  If the node is a category section, I pop off the end of the sorted array, --> ['Womens', 15] for example, then find in the refs object the HTML that corresponds to the 0th index of that popped off value --> refs[sorted.pop()[0]] --> refs[['Womens', 15]] --> refs['Womens'] === html of the Womens module. I then swap that HTML for the HTML currently within modules[i]
+
 for (let i = 0; i < modules.length; i++) {
   if (modules[i].className === 'o-row   ') {
     let html = refs[sorted.pop()[0]];
--- extension/productPage.js
+++ extension/productPage.js
@@ -1,4 +1,4 @@
-//I couldnt get utag_data to work so went with parsing the path at the top of each product to determine it's category
+//I couldnt get utag_data to work so went with parsing the path at the top of each product to determine it's category.  There are more categories than just Womens, Beauty, Home, Lifestyle, and Mens that don't exist on the New -Arrivals page, such as On Campus, so I store those too but dont actually do anything with them.
 let path = document.getElementsByClassName('c-breadcrumb')[0].outerText;
 let category = path.split(' ')[0];
 
@@ -8,8 +8,9 @@ let addToCart = Array.from(
 )[0];
 
 /*
-Increase storage function that takes the category (from above), and the type (page-view or add-to-cart)
-I wasn't sure what to name CSE_Challenge due to the 5 categories. I had trouble creating a CSE_Challenge object on window.localStorage to hold the various categories so created them on local storage directly.
+Below is thew increase storage function that takes the category (from above), and the type (page-view or add-to-cart).  The type represents how much to increase the storage by.
+
+I wasn't sure what variable to name CSE_Challenge due to the 5 categories so I tried to creat an object on localStorage where I could nest the affinity values. I had trouble creating a CSE_Challenge object on window.localStorage so held the affinities directly on localStorage.
 */
 
 const increaseStorage = (cat, type) => {
@@ -23,10 +24,10 @@ const increaseStorage = (cat, type) => {
   }
 };
 
-//increase the storage immediately for any page view
+//Invoke the function with type === 'page-view' as to increase the storage immediately for any page view.
 increaseStorage(category, 'page-view');
 
-//Event listener for add to cart button
+//Event listener for add to cart button that invokes the function with type === 'add-to-cart' as to increase the storage by 3
 addToCart.addEventListener('click', () =>
   increaseStorage(category, 'add-to-cart')
 );
